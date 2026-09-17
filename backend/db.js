12df240f33c1772c2bc76771c3039544c5db60cb
reskiiu <qjuanpablo3@gmail.com>
2026-09-16 20:33:05 -0500

    server

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/boda')
-    .then(() => console.log('Conectado a la BD "boda"'))
+// El secreto está aquí: process.env.MONGODB_URI lo usará Render. Si no existe, usa tu localhost.
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/boda';
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Conectado a MongoDB correctamente'))
     .catch((err) => console.error(err));
 
 const invitadoSchema = new mongoose.Schema({
@@ -13,6 +16,6 @@ const invitadoSchema = new mongoose.Schema({
         default: 'Pendiente' 
     },
     limiteModificacion: { type: Date, required: true }
-}, { collection: 'invitados' }); // <-- Apunta exactamente a tu colección
+}, { collection: 'invitados' });
 
 module.exports = mongoose.model('Invitado', invitadoSchema);
